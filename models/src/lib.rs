4d2232f551daae61e97a2f6c785f099e24f7d14d
Christerpher Hunter <djhunter67@gmail.com>
2023-12-28 09:08:00 -0500

    add: templates and static dirs

--- models/src/lib.rs
+++ models/src/lib.rs
@@ -1,4 +1,31 @@
+use std::fmt::{self, Display, Formatter};
+
 pub struct Person {
     pub name: String,
     pub age: u8,
+    pub email: String,
+}
+
+impl Display for Person {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "Name: {}, Age: {}", self.name, self.age)
+    }
+}
+
+pub struct History {
+    pub title: &'static str,
+    pub history: Vec<String>,
+}
+
+impl Display for History {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "Title: {}, History: {:?}", self.title, self.history[0])
+    }
+}
+
+pub struct FastingInfo {
+    pub notes: String,
+    pub weight_time: String,
+    pub weight: f32,
+    pub time_since_last_meal: String,
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,29 +1,79 @@
 use std::fs::File;
 
-use actix_web::{self, get, middleware, App, HttpResponse, HttpServer, Responder};
+use actix_files::NamedFile;
+use actix_web::{
+    self, get, guard, http, middleware, web, App, Error, HttpResponse, HttpServer, Responder,
+};
+use askama::Template;
 use client::{self, add_1};
-use models::{self, Person};
-use simplelog::{CombinedLogger, LevelFilter, WriteLogger};
+use models::{self, History, Person};
+use simplelog::{
+    ColorChoice, CombinedLogger, Config, LevelFilter, TermLogger, TerminalMode, WriteLogger,
+};
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    title: &'static str,
+    people: Person,
+}
+
+#[derive(Template)]
+#[template(path = "history.html")]
+struct HistoryTemplate {
+    title: &'static str,
+    history: History,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    match CombinedLogger::init(vec![WriteLogger::new(
-        LevelFilter::Debug,
-        simplelog::Config::default(),
-        File::create("wifi_rs.log").unwrap(),
-    )]) {
-        Ok(_) => log::debug!("Logger initialized"),
-        Err(e) => log::debug!("Logger failed to initialize: {}", e),
+    match CombinedLogger::init(vec![
+        TermLogger::new(
+            LevelFilter::Debug,
+            Config::default(),
+            TerminalMode::Stdout,
+            ColorChoice::Always,
+        ),
+        WriteLogger::new(
+            LevelFilter::Trace,
+            Config::default(),
+            File::create("faster_tracker.log")?,
+        ),
+    ]) {
+        // This is a macro that allows for multiple loggers to be used at once
+        Ok(_) => log::debug!("Logger initialized."),
+        Err(e) => {
+            println!("Error: {}", e);
+            std::process::exit(1)
+        }
     }
 
     // Start the web server.
     HttpServer::new(|| {
-        App::new().wrap(middleware::Logger::default())
-        // .route("/", web::get().to(root))
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(actix_files::Files::new("/static", "static").show_files_listing())
+            .service(root)
+            .service(history)
+            .service(
+                web::resource("/favicon.ico")
+                    .guard(guard::Method(http::Method::GET))
+                    .to(send_favicon_icon),
+            )
+            .service(
+                web::resource("/index.css")
+                    .guard(guard::Method(http::Method::GET))
+                    .to(send_css_file),
+            )
+            .service(
+                web::resource("/bulma-helpers.min.css")
+                    .guard(guard::Method(http::Method::GET))
+                    .to(serve_bulma_css),
+            )
     })
-    .bind(("127.0.0.1", 8000))
+    .bind(("127.0.0.1", 8080))
     .unwrap_or_else(|_| {
-        log::warn!("Can not bind to port 8081");
+        log::warn!("Can not bind to port 8080");
         std::process::exit(1)
     })
     .run()
@@ -33,7 +83,54 @@ async fn main() -> std::io::Result<()> {
 #[get("/")]
 async fn root() -> impl Responder {
     // Return './templates/index.html'.
+
+    let people = IndexTemplate {
+        title: "Faster Tracker",
+        people: Person {
+            name: "Hunter, Christerpher".to_string(),
+            age: 34,
+            email: "djhunter67@gmail.com".to_string(),
+        },
+    };
+
     HttpResponse::Ok()
         .content_type("text/html")
-        .body(include_str!("../templates/index.html"))
+        .body(people.render().unwrap())
+}
+
+#[get("/history")]
+async fn history() -> impl Responder {
+    let history = HistoryTemplate {
+        title: "Faster Tracker",
+        history: History {
+            title: "History",
+            history: vec![
+                "This is the first line".to_string(),
+                "This is the second line".to_string(),
+                "This is the third line".to_string(),
+            ],
+        },
+    };
+
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(history.render().unwrap())
+}
+
+pub async fn send_favicon_icon() -> Result<NamedFile, Error> {
+    Ok(NamedFile::open(
+        "static/images/do-not-eat-or-drink-here.svg",
+    )?)
+}
+
+async fn send_css_file() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/css")
+        .body(include_str!("../static/css/index.css"))
+}
+
+async fn serve_bulma_css() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/css")
+        .body(include_str!("../static/css/bulma-helpers.min.css"))
 }
